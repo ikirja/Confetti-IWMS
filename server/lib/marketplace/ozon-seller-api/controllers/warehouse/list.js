b86f0d9e22192f07b5f8e096bd0868af3b79bc01
Kirill Makeev <kirill.makeev@icloud.com>
2022-04-08 22:29:13 +0300

    refactor

--- server/lib/marketplace/ozon-seller-api/controllers/warehouse/list.js
+++ server/lib/marketplace/ozon-seller-api/controllers/warehouse/list.js
@@ -1,6 +1,6 @@
 const request = require('../request');
 
-module.exports = async function warehouseList(task_id) {
+module.exports = async function warehouseList() {
   const response = await request('/v1/warehouse/list', 'POST', {});
   if (response.status === 200) return response.json();
   return response;
