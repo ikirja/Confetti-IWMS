f23ac252228d8513e7528da7796746b8f40e5780
Kirill Makeev <kirill.makeev@icloud.com>
2022-04-19 23:10:15 +0300

    new logo

--- client/src/components/layout/LeftSidebar.vue
+++ client/src/components/layout/LeftSidebar.vue
@@ -3,24 +3,20 @@
     <!-- LOGO -->
     <a href="/" class="logo text-center logo-light">
       <span class="logo-lg">
-        <!-- <img src="assets/images/logo.png" alt="" height="16" /> -->
-        Confetti
+        <img src="assets/images/logo-confetti.png" alt="" height="75" />
       </span>
       <span class="logo-sm">
-        <!-- <img src="assets/images/logo_sm.png" alt="" height="16" /> -->
-        Confetti
+        <img src="assets/images/logo-confetti.png" alt="" height="50" />
       </span>
     </a>
 
     <!-- LOGO -->
     <a href="/" class="logo text-center logo-dark">
       <span class="logo-lg">
-        <!-- <img src="assets/images/logo-dark.png" alt="" height="16" /> -->
-        Confetti
+        <img src="assets/images/logo-confetti.png" alt="" height="75" />
       </span>
       <span class="logo-sm">
-        <!-- <img src="assets/images/logo_sm_dark.png" alt="" height="16" /> -->
-        Confetti
+        <img src="assets/images/logo-confetti.png" alt="" height="50" />
       </span>
     </a>
 
